6e459ed3df26561a5e0efab67a2170459e12ec3f
byeongkwon <hanbg0@gmail.com>
2019-08-24 16:56:56 +0900

    리팩토링 - Player, Counter 컴포넌트를 별도의 화일로 분리

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
-
-class Counter extends React.Component { // extends React.Component : 부모가 갖고있는 속성과 메서드를 다 물려받음
-  state = {
-    score: 0,
-    a: 10
-  }
-
-  constructor() {
-    super();
-    this.handleChangeScore = this.handleChangeScore.bind(this);
-  }
-
-  handleChangeScore(delta) {
-    console.log('incrementScore', this);
-    this.setState(prevState => ({score: prevState.score + delta}));
-  }
-
-  // 이벤트의 오른쪽은 함수 선언문이 들어가야 한다.
-  render() {
-    return (
-      <div className="counter">
-        <button className="counter-action decrement" onClick={() => this.handleChangeScore(-1)}> -</button>
-        <span className="counter-score">{this.state.score}</span>
-        <button className="counter-action increment" onClick={() => this.handleChangeScore(1)}> +</button>
-      </div>
-    )
-  }
-}
-const Player = (props) => (
-  <div className="player">
-		<span className="player-name">
-			<button className="remove-player" onClick={() => props.removePlayer(props.id)}> X </button>
-      {props.name}
-		</span>
-    <Counter />
-  </div>
-);
+import { Player } from './components/Player';
 
 class App extends React.Component {
   state = {
